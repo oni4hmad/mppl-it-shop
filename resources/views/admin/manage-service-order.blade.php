b181cb8c7560e82e94bdd2db7db5ab682786829a
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2021-12-12 17:51:13 +0700

    style: modal->catatan teknisi

--- resources/views/admin/manage-service-order.blade.php
+++ resources/views/admin/manage-service-order.blade.php
@@ -78,60 +78,166 @@
 
         {{-- order cards --}}
         <div class="row border-end p-2">
-          @for ($i = 0; $i < 3; $i++)
-            {{-- card-1 --}}
-            <div class="col-12 p-3 mb-2 rounded-3 border">
-              {{-- status, id, user, waktu order --}}
-              <div class="d-flex flex-row align-items-center mb-3">
-                <p class="mb-0 px-0 me-4 fw-bold text-break text-warning">Mencari Teknisi</p>
-                <p class="mb-0 px-0 me-4 fw-bold text-break text-primary">ID: 12001</p>
-                <i class="far fa-user me-2 text-secondary"></i>
-                <p class="mb-0 px-0 me-4 text-break text-secondary">Fiody Ardhito</p>
-                <i class="far fa-clock me-2 text-secondary"></i>
-                <p class="mb-0 px-0 me-4 text-break text-secondary">29 Juni 2021 - 10:30 WIB</p>
-              </div>
-              <div class="col-12 mb-3">
-                <div class="row mx-0">
-                  <div class="col-5 ps-0 border-end">
-                    <div class="row">
-                      <p class="mb-0 fw-bold text-break">Servis:</p>
-                      <p class="mb-0 text-break">Laptop suka tiba-tiba mati sendiri padahal baterai masih penuh dan kadang bisa di cas kadang tidak bisa</p>
-                    </div>
-                  </div>
-                  <div class="col-4 border-end">
-                    <div class="row">
-                      <p class="mb-0 fw-bold text-break">Alamat</p>
-                      <p class="mb-0 text-break">Fiody Ardhito (089512341234)<br>Jl. Manukan Indah II 19C/8, Kec. Tandes, Kota Surabaya, Jawa Timur, 60185</p>
-                    </div>
-                  </div>
-                  <div class="col-3">
-                    <div class="row">
-                      <p class="mb-0 fw-bold text-break">Permintaan Jadwal</p>
-                      <p class="mb-0 text-break">Selasa, 29 Juni 2021</p>
-                      <p class="mb-0 text-break">Jam 12.00</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
+
+          {{-- card-1 --}}
+          <div class="col-12 p-3 mb-2 rounded-3 border">
+            {{-- status, id, user, waktu order --}}
+            <div class="d-flex flex-row align-items-center mb-3">
+              <p class="mb-0 px-0 me-4 fw-bold text-break text-warning">Mencari Teknisi</p>
+              <p class="mb-0 px-0 me-4 fw-bold text-break text-primary">ID: 12001</p>
+              <i class="far fa-user me-2 text-secondary"></i>
+              <p class="mb-0 px-0 me-4 text-break text-secondary">Fiody Ardhito</p>
+              <i class="far fa-clock me-2 text-secondary"></i>
+              <p class="mb-0 px-0 me-4 text-break text-secondary">29 Juni 2021 - 10:30 WIB</p>
+            </div>
+            <div class="col-12 mb-3">
               <div class="row mx-0">
-                <div class="col px-3 py-1 rounded-3 border bg-light">
+                <div class="col-5 ps-0 border-end">
                   <div class="row">
-                    <div class="col-6 d-flex flex-row">
-                      <p class="mb-0 me-1 px-0 fw-bold text-break">Teknisi:</p>
-                      <p class="mb-0 px-0 fw-bold text-break me-4">-</p>
-                    </div>
-                    <div class="col-6 d-flex flex-row">
-                      <p class="mb-0 me-1 px-0 fw-bold text-break">Total Biaya:</p>
-                      <p class="mb-0 px-0 fw-bold text-break me-4">-</p>
-                    </div>
+                    <p class="mb-0 fw-bold text-break">Servis:</p>
+                    <p class="mb-0 text-break">Laptop suka tiba-tiba mati sendiri padahal baterai masih penuh dan kadang bisa di cas kadang tidak bisa</p>
                   </div>
                 </div>
-                <div class="col-auto ps-2 pe-0">
-                  <button type="button" class="btn btn-sm btn-primary rounded-3 px-5 h-100 fw-bold">Cari Teknisi</button>
+                <div class="col-4 border-end">
+                  <div class="row">
+                    <p class="mb-0 fw-bold text-break">Alamat</p>
+                    <p class="mb-0 text-break">Fiody Ardhito (089512341234)<br>Jl. Manukan Indah II 19C/8, Kec. Tandes, Kota Surabaya, Jawa Timur, 60185</p>
+                  </div>
+                </div>
+                <div class="col-3">
+                  <div class="row">
+                    <p class="mb-0 fw-bold text-break">Permintaan Jadwal</p>
+                    <p class="mb-0 text-break">Selasa, 29 Juni 2021</p>
+                    <p class="mb-0 text-break">Jam 12.00</p>
+                  </div>
                 </div>
               </div>
             </div>
-          @endfor
+            <div class="row mx-0">
+              <div class="col px-3 py-1 rounded-3 border bg-light">
+                <div class="row">
+                  <div class="col-6 d-flex flex-row">
+                    <p class="mb-0 me-1 px-0 fw-bold text-break">Teknisi:</p>
+                    <p class="mb-0 px-0 fw-bold text-break me-4">-</p>
+                  </div>
+                  <div class="col-6 d-flex flex-row">
+                    <p class="mb-0 me-1 px-0 fw-bold text-break">Total Biaya:</p>
+                    <p class="mb-0 px-0 fw-bold text-break me-4">-</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-auto ps-2 pe-0">
+                <button type="button" class="btn btn-sm btn-primary rounded-3 h-100 fw-bold" style="min-width: 200px;" onclick="location.href='manage-technician'">Cari Teknisi</button>
+              </div>
+            </div>
+          </div>
+
+          {{-- card-2 --}}
+          <div class="col-12 p-3 mb-2 rounded-3 border">
+            {{-- status, id, user, waktu order --}}
+            <div class="d-flex flex-row align-items-center mb-3">
+              <p class="mb-0 px-0 me-4 fw-bold text-break text-warning">Dalam Servis</p>
+              <p class="mb-0 px-0 me-4 fw-bold text-break text-primary">ID: 12000</p>
+              <i class="far fa-user me-2 text-secondary"></i>
+              <p class="mb-0 px-0 me-4 text-break text-secondary">Fiody Ardhito</p>
+              <i class="far fa-clock me-2 text-secondary"></i>
+              <p class="mb-0 px-0 me-4 text-break text-secondary">29 Juni 2021 - 10:30 WIB</p>
+            </div>
+            <div class="col-12 mb-3">
+              <div class="row mx-0">
+                <div class="col-5 ps-0 border-end">
+                  <div class="row">
+                    <p class="mb-0 fw-bold text-break">Servis:</p>
+                    <p class="mb-0 text-break">Laptop suka tiba-tiba mati sendiri padahal baterai masih penuh dan kadang bisa di cas kadang tidak bisa</p>
+                  </div>
+                </div>
+                <div class="col-4 border-end">
+                  <div class="row">
+                    <p class="mb-0 fw-bold text-break">Alamat</p>
+                    <p class="mb-0 text-break">Fiody Ardhito (089512341234)<br>Jl. Manukan Indah II 19C/8, Kec. Tandes, Kota Surabaya, Jawa Timur, 60185</p>
+                  </div>
+                </div>
+                <div class="col-3">
+                  <div class="row">
+                    <p class="mb-0 fw-bold text-break">Permintaan Jadwal</p>
+                    <p class="mb-0 text-break">Selasa, 29 Juni 2021</p>
+                    <p class="mb-0 text-break">Jam 12.00</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row mx-0">
+              <div class="col px-3 py-1 rounded-3 border bg-light">
+                <div class="row">
+                  <div class="col-6 d-flex flex-row">
+                    <p class="mb-0 me-1 px-0 fw-bold text-break">Teknisi:</p>
+                    <p class="mb-0 px-0 fw-bold text-break me-4">-</p>
+                  </div>
+                  <div class="col-6 d-flex flex-row">
+                    <p class="mb-0 me-1 px-0 fw-bold text-break">Total Biaya:</p>
+                    <p class="mb-0 px-0 fw-bold text-break me-4">-</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-auto ps-2 pe-0">
+                <button type="button" class="btn btn-sm btn-secondary rounded-3 h-100 fw-bold" style="min-width: 200px;" disabled>Cari Teknisi</button>
+              </div>
+            </div>
+          </div>
+
+          {{-- card-3 --}}
+          <div class="col-12 p-3 mb-2 rounded-3 border">
+            {{-- status, id, user, waktu order --}}
+            <div class="d-flex flex-row align-items-center mb-3">
+              <p class="mb-0 px-0 me-4 fw-bold text-break text-secondary">Servis Selesai</p>
+              <p class="mb-0 px-0 me-4 fw-bold text-break text-primary">ID: 11999</p>
+              <i class="far fa-user me-2 text-secondary"></i>
+              <p class="mb-0 px-0 me-4 text-break text-secondary">Fiody Ardhito</p>
+              <i class="far fa-clock me-2 text-secondary"></i>
+              <p class="mb-0 px-0 me-4 text-break text-secondary">29 Juni 2021 - 10:30 WIB</p>
+            </div>
+            <div class="col-12 mb-3">
+              <div class="row mx-0">
+                <div class="col-5 ps-0 border-end">
+                  <div class="row">
+                    <p class="mb-0 fw-bold text-break">Servis:</p>
+                    <p class="mb-0 text-break">Laptop suka tiba-tiba mati sendiri padahal baterai masih penuh dan kadang bisa di cas kadang tidak bisa</p>
+                  </div>
+                </div>
+                <div class="col-4 border-end">
+                  <div class="row">
+                    <p class="mb-0 fw-bold text-break">Alamat</p>
+                    <p class="mb-0 text-break">Fiody Ardhito (089512341234)<br>Jl. Manukan Indah II 19C/8, Kec. Tandes, Kota Surabaya, Jawa Timur, 60185</p>
+                  </div>
+                </div>
+                <div class="col-3">
+                  <div class="row">
+                    <p class="mb-0 fw-bold text-break">Permintaan Jadwal</p>
+                    <p class="mb-0 text-break">Selasa, 29 Juni 2021</p>
+                    <p class="mb-0 text-break">Jam 12.00</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row mx-0">
+              <div class="col px-3 py-1 rounded-3 border bg-light">
+                <div class="row">
+                  <div class="col-6 d-flex flex-row">
+                    <p class="mb-0 me-1 px-0 fw-bold text-break">Teknisi:</p>
+                    <p class="mb-0 px-0 fw-bold text-break me-4">-</p>
+                  </div>
+                  <div class="col-6 d-flex flex-row">
+                    <p class="mb-0 me-1 px-0 fw-bold text-break">Total Biaya:</p>
+                    <p class="mb-0 px-0 fw-bold text-break me-4">-</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-auto ps-2 pe-0">
+                <button type="button" class="btn btn-sm btn-outline-primary rounded-3 h-100 fw-bold" style="min-width: 200px;" data-bs-toggle="modal" data-bs-target="#modal-catatan-teknisi">Catatan Teknisi</button>
+              </div>
+            </div>
+          </div>
+
         </div>
 
         {{-- pagination --}}
@@ -154,6 +260,35 @@
         </div>
       </div>
 
+    </div>
+  </div>
+
+  <!--------------------------------------->
+  <!---------------- MODAL ---------------->
+  <!--------------------------------------->
+  {{-- data-bs-toggle="modal" data-bs-target="#modal-" --}}
+
+  <!-- modal: catatan teknisi -->
+  <div class="modal fade" id="modal-catatan-teknisi" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <div class="row">
+            <h5 class="modal-title" id="staticBackdropLabel">Catatan Teknisi (Order ID: 11999)</h5>
+            <p class="mb-0 text-secondary">Diservis oleh: Budi Ramadhan</p>
+          </div>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p class="mb-0 fw-bold">Total Biaya</p>
+          <input id="" type="text" value="150000" class="form-control mb-3" required autocomplete="name" autofocus placeholder="Total biaya" disabled>
+          <p class="mb-0 fw-bold">Rincian Servis</p>
+          <textarea id="" type="text" class="form-control" name="name" rows="3" required autocomplete="name" autofocus placeholder="Rincian servis" disabled>tidak ada penggantian perangkat keras komputer, hanya pembersihan komputer, pembaruan thermal paste, dan install ulang windows (total Rp150.000)</textarea>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary btn-sm px-5 text-white fw-bold" data-bs-dismiss="modal">OK</button>
+        </div>
+      </div>
     </div>
   </div>
 
