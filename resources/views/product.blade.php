f3d9117e42a33b9be75609106175755a56fab6d7
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2023-03-06 02:50:16 +0700

    add and link balas, ubah, delete ulasan modals

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -134,11 +134,10 @@
                     <span class="visually-hidden">Toggle Dropdown</span>
                   </button>
                   <ul class="dropdown-menu dropdown-menu-lg-end">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#modal-balas-ulasan">Balas</a></li>
+                    <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#modal-nilai-produk">Ubah</a></li>
                     <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Separated link</a></li>
+                    <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#modal-hapus-ulasan">Hapus</a></li>
                   </ul>
                 </div>
               </div>
@@ -168,6 +167,7 @@
 
           </div>
         </div>
+
       </div>
 
       {{-- atur jumlah --}}
@@ -203,6 +203,153 @@
         </div>
       </div>
 
+    </div>
+  </div>
+
+
+  <!--------------------------------------->
+  <!---------------- MODAL ---------------->
+  <!--------------------------------------->
+  <!-- data-bs-toggle="modal" data-bs-target="#modal-editxxx" -->
+
+  <!-- modal: berhasil ditambahkan ke keranjang -->
+  <div class="modal fade" id="modal-berhasil-masuk-keranjang" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-sm modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title w-100 text-center" id="staticBackdropLabel">Berhasil Ditambahkan</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="row mb-2">
+            <div class="col">
+              <p class="mb-0 text-center">Item berhasil ditambahkan ke keranjang.</p>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary btn-sm p-2 fw-bold w-100" onclick="{{ "location.href = 'cart';" }}">Lihat Keranjang</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- modal: nilai produk -->
+  <div class="modal fade" id="modal-nilai-produk" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel">Nilai Produk</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          {{-- produknya --}}
+          <div class="row mx-0 py-3">
+            {{-- image --}}
+            <div class="col-auto px-0 rounded-3">
+              <div class="p-0 me-1">
+                <div style="width: 6.5rem; height: 6.5rem;">
+                  <div class="w-100 h-100 rounded-3 border border-secondary" style="background-image: url('https://picsum.photos/150/510'); background-size: cover; background-position: center center;"></div>
+                </div>
+              </div>
+            </div>
+            {{-- product name --}}
+            <div class="col d-flex flex-column justify-content-between">
+              <div class="row justify-content-between">
+                <div class="col-auto">
+                  <p class="mb-0 fw-bolder text-break">VGA MSI GT1030 AERO ITX 2G OC | GT 1030</p>
+                  <p class="mb-0 px-0 text-break">1 barang x Rp4.000.000</p>
+                  <p class="mb-0 text-secondary text-break">3 Juni 2021</p>
+                  <div class="d-flex flex-row align-items-center">
+                    <p class="mb-0 me-2 text-primary fw-bold">Rating (1-5):</p>
+                    <input type="number" name="" id="" min="1" max="5" value="4">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <textarea id="" type="text" class="form-control" name="name" rows="3" required autocomplete="name" autofocus placeholder="Tambahkan review">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo accusantium reprehenderit possimus laborum nemo</textarea>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary btn-sm px-3 text-white fw-bold" data-bs-dismiss="modal">Batal</button>
+          <button type="button" class="btn btn-primary btn-sm px-3 fw-bold">Ubah</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- modal: balas ulasan -->
+  <div class="modal fade" id="modal-balas-ulasan" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel">Balas Ulasan</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          {{-- ulasan pengguna --}}
+          <div class="row pb-3 mb-3 border-bottom">
+            {{-- profile --}}
+            <div class="col" style="max-width: 250px;">
+              <div class="row">
+                <div class="col" style="max-width: 80px;">
+                  <div style="width: 60px; height: 60px;">
+                    <div class="w-100 h-100 bg-image rounded-circle border" style="background-image: url('https://picsum.photos/150/510'); background-size: cover; background-position: center center;"></div>
+                  </div>
+                </div>
+                <div class="col">
+                  <p class="m-0 p-0 fw-bold text-break">OniAhmad</p>
+                  <p class="m-0 p-0 text-secondary text-break">5 jam yang lalu</p>
+                </div>
+              </div>
+            </div>
+            {{-- isi ulasan --}}
+            <div class="col m-0 p-0">
+              {{-- bintang --}}
+              <div class="row">
+                <div class="col mb-2">
+                  @for ($i = 0; $i < 4; $i++)
+                    <i class="fas fa-star text-warning me-1"></i> {{-- kuning --}}
+                  @endfor
+                  @for ($i = 0; $i < 1; $i++)
+                    <i class="fas fa-star text-secondary me-1"></i> {{-- abu-abu --}}
+                  @endfor
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {{-- teks ulasan --}}
+          <div class="row border-bottom px-3 pb-3 mb-3">
+            <p class="p-0 m-0 text-break">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo accusantium reprehenderit possimus laborum nemo, cumque nostrum veniam assumenda! Aut veniam amet quis? Hic fugit cumque sapiente? Tenetur corrupti dignissimos est dolor harum. Debitis sint tenetur incidunt nam molestias ratione illum, aperiam consequuntur adipisci vel rerum autem excepturi?</p>
+          </div>
+          <p class="p-0 m-0 fw-bold text-break">Balasan:</p>
+          <textarea id="" type="text" class="form-control" name="name" rows="3" required autocomplete="name" autofocus placeholder="Balasan"></textarea>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary btn-sm px-3 text-white fw-bold" data-bs-dismiss="modal">Batal</button>
+          <button type="button" class="btn btn-primary btn-sm px-3 fw-bold">Kirim</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- modal: hapus ulasan -->
+  <div class="modal fade" id="modal-hapus-ulasan" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-sm modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title w-100 text-center" id="staticBackdropLabel">Hapus Ulasan?</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p class="mb-0 text-center">Konfirmasi penghapusan ulasan.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary btn-sm p-1 fw-bold w-100">Hapus</button>
+          <button type="button" class="btn btn-secondary btn-sm p-1 text-white fw-bold w-100" data-bs-dismiss="modal">Batal</button>
+        </div>
+      </div>
     </div>
   </div>
 
