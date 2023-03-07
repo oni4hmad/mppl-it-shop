2b82e14160b0c03166434dbfced8356934d37478
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2023-03-08 00:26:33 +0700

    add user type middleware

---
+++ app/Enums/UserType.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static OptionOne()
+ * @method static static OptionTwo()
+ * @method static static OptionThree()
+ */
+final class UserType extends Enum
+{
+    const CUSTOMER = 0;
+    const ADMINISTRATOR = 1;
+    const TECHNICIAN = 2;
+}
