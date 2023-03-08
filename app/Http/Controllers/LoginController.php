7614ae86ba2b960bab9742cb7f7e7a050e5dc04a
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2023-03-08 11:08:40 +0700

    update login failed message

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -20,7 +20,7 @@ class LoginController extends Controller
         $credentials = $request->only('email', 'password');
         if (!Auth::attempt($credentials)) {
             return redirect('/login')
-                ->with(['error' => 'Login gagal.']);
+                ->with(['error' => 'Email atau password salah.']);
         }
         return redirect('/');
     }
