9e51f83268a30a7ea40434991b574b129b6e041a
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2023-03-11 13:49:51 +0700

    add technician enums

---
+++ app/Enums/TechnicianStatus.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static OptionOne()
+ * @method static static OptionTwo()
+ * @method static static OptionThree()
+ */
+final class TechnicianStatus extends Enum
+{
+    const TERSEDIA = '0';
+    const MENUNGGU_KONFIRMASI = '1';
+    const TIDAK_TERSEDIA = '2';
+}
