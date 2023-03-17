b266b8e04417c9cee4a6c085d7bfbd32890ccefd
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2023-03-17 16:31:15 +0700

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -15,7 +15,7 @@
         <div class="d-flex flex-column justify-content-center align-items-center h-100">
           <h1 class="fw-bold text-primary" style="font-size: 10rem;">404</h1>
           <p class="mb-2 fw-bold fs-5">Page not found.</p>
-          <p class="fw-bold fs-6 text-secondary text-center">This Page you are looking for doesn't exists. <a href="{{ url()->previous() }}">Go back</a>, or head over to our customer to ask for assistance.</p>
+          <p class="fw-bold fs-6 text-secondary text-center">This page you are looking for doesn't exists.<br><a href="{{ url()->previous() }}">Go back</a>, or head over to our customer service to ask for assistance.</p>
         </div>
       </div>
     </div>
