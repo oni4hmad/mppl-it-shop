f3a127f5015ca3cef0c4a5e1238e70f020229997
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2023-03-20 09:14:26 +0700

    change toast script and its elements to existed partials

--- resources/views/order-history-product.blade.php
+++ resources/views/order-history-product.blade.php
@@ -29,53 +29,8 @@
     }
   </style>
 
-  <script>
-    window.onload = () => {
-      @if ($message = Session::get('success'))
-        {{--show success toast--}}
-        let toastSuccessEl = document.getElementById('toast-success');
-        let changeSuccessText = (text) => toastSuccessEl.querySelector('.toast-body').innerHTML = `${text}`;
-        let toastSuccess = new bootstrap.Toast(toastSuccessEl);
-        changeSuccessText("{{ $message }}");
-        toastSuccess.show();
-      @elseif ($errors->any())
-        {{--show error validation toast--}}
-        let toastErrorEl = document.getElementById('toast-error');
-        let changeErrorText = (text) => toastErrorEl.querySelector('.toast-body').innerHTML = `${text}`;
-        let toastError = new bootstrap.Toast(toastErrorEl);
-        let message = "Pengisian gagal:";
-        @foreach ($errors->all() as $error)
-          message += "<li>{{ $error }}</li>";
-        @endforeach
-        changeErrorText(message);
-        toastError.show();
-      @endif
-    };
-  </script>
-
-  {{--toast success--}}
-  <div class="toast-container position-fixed p-3 py-5 bottom-0 start-50 translate-middle-x z-3">
-    <div id="toast-success" class="toast align-items-center bg-success border-0" role="alert" aria-live="assertive" aria-atomic="true">
-      <div class="d-flex">
-        <div class="toast-body text-white">
-          Hello, world! This is a toast message.
-        </div>
-        <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-      </div>
-    </div>
-  </div>
-
-  {{--toast error--}}
-  <div class="toast-container position-fixed p-3 py-5 bottom-0 start-50 translate-middle-x z-3">
-    <div id="toast-error" class="toast align-items-center bg-danger border-0" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="300000">
-      <div class="d-flex">
-        <div class="toast-body text-white">
-          Hello, world! This is a toast message.
-        </div>
-        <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-      </div>
-    </div>
-  </div>
+  {{-- error/errors/success toast --}}
+  @include('partials.toast-error-success')
 
   <div class="container">
     <div class="row">
@@ -256,7 +211,6 @@
           </nav>
         </div>
       </div>
-
     </div>
   </div>
 
@@ -280,7 +234,7 @@
               <div class="modal-body">
                 <p class="mb-0 text-center">Apakah kamu yakin ingin membatalkan pesanan?</p>
               </div>
-              <form action="/cancel-order-product/{{ $productOrder->id }}" method="post">
+              <form action="/order-product/cancel/{{ $productOrder->id }}" method="post">
                 @csrf
                 <div class="modal-footer">
                   <button type="submit" class="btn btn-primary btn-sm p-1 fw-bold w-100">Iya</button>
