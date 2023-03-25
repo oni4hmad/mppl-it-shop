6633d841d6f9683a8aa4b81cd438d455127680aa
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2023-03-26 03:33:01 +0700

    fix admin sidebar path

--- resources/views/partials/admin-sidebar.blade.php
+++ resources/views/partials/admin-sidebar.blade.php
@@ -24,26 +24,26 @@
           <a href="/dashboard" class="text-decoration-none"><p id="side-nav" class="mb-0 p-2 fw-bold text-break {{ Request::segment(1)=='dashboard' ? "text-decoration-underline" : "text-secondary" }}">
             <i class="fas fa-home me-2"></i>Home</p>
           </a>
-          <a href="manage-product" class="text-decoration-none"><p id="side-nav" class="mb-0 p-2 fw-bold text-break {{ Request::segment(1)=='manage-product' ? "text-decoration-underline" : "text-secondary" }}">
+          <a href="/manage-product" class="text-decoration-none"><p id="side-nav" class="mb-0 p-2 fw-bold text-break {{ Request::segment(1)=='manage-product' ? "text-decoration-underline" : "text-secondary" }}">
             <i class="fas fa-microchip me-2"></i>Produk</p>
           </a>
-          <a href="manage-category" class="text-decoration-none"><p id="side-nav" class="mb-0 p-2 fw-bold text-break {{ Request::segment(1)=='manage-category' ? "text-decoration-underline" : "text-secondary" }}">
+          <a href="/manage-category" class="text-decoration-none"><p id="side-nav" class="mb-0 p-2 fw-bold text-break {{ Request::segment(1)=='manage-category' ? "text-decoration-underline" : "text-secondary" }}">
             <i class="fa-solid fa-th-large me-2"></i>Kategori</p>
           </a>
-          <a href="manage-payment-method" class="text-decoration-none"><p id="side-nav" class="mb-0 p-2 fw-bold text-break {{ Request::segment(1)=='manage-payment-method' ? "text-decoration-underline" : "text-secondary" }}">
+          <a href="/manage-payment-method" class="text-decoration-none"><p id="side-nav" class="mb-0 p-2 fw-bold text-break {{ Request::segment(1)=='manage-payment-method' ? "text-decoration-underline" : "text-secondary" }}">
             <i class="fas fa-money-check-alt me-2"></i>Metode Pembayaran</p>
           </a>
-          <a href="manage-courier" class="text-decoration-none"><p id="side-nav" class="mb-0 p-2 fw-bold text-break {{ Request::segment(1)=='manage-courier' ? "text-decoration-underline" : "text-secondary" }}">
+          <a href="/manage-courier" class="text-decoration-none"><p id="side-nav" class="mb-0 p-2 fw-bold text-break {{ Request::segment(1)=='manage-courier' ? "text-decoration-underline" : "text-secondary" }}">
             <i class="fa-solid fa-truck-fast me-2"></i>Kurir</p>
           </a>
-          <a href="manage-technician" class="text-decoration-none"><p id="side-nav" class="mb-0 p-2 fw-bold text-break {{ Request::segment(1)=='manage-technician' ? "text-decoration-underline" : "text-secondary" }}">
+          <a href="/manage-technician" class="text-decoration-none"><p id="side-nav" class="mb-0 p-2 fw-bold text-break {{ Request::segment(1)=='manage-technician' ? "text-decoration-underline" : "text-secondary" }}">
             <i class="fas fa-user-cog me-2"></i>Teknisi</p>
           </a>
           <p class="my-3 p-2 py-3 border-top border-bottom fw-bold text-break text-secondary">Orderan</p>
-          <a href="manage-product-order" class="text-decoration-none"><p id="side-nav" class="mb-0 p-2 fw-bold text-break {{ Request::segment(1)=='manage-product-order' ? "text-decoration-underline" : "text-secondary" }}">
+          <a href="/manage-product-order" class="text-decoration-none"><p id="side-nav" class="mb-0 p-2 fw-bold text-break {{ Request::segment(1)=='manage-product-order' ? "text-decoration-underline" : "text-secondary" }}">
             <i class="fas fa-server me-2"></i>Produk Elektronik</p>
           </a>
-          <a href="manage-service-order" class="text-decoration-none"><p id="side-nav" class="mb-0 p-2 fw-bold text-break {{ Request::segment(1)=='manage-service-order' ? "text-decoration-underline" : "text-secondary" }}">
+          <a href="/manage-service-order" class="text-decoration-none"><p id="side-nav" class="mb-0 p-2 fw-bold text-break {{ Request::segment(1)=='manage-service-order' ? "text-decoration-underline" : "text-secondary" }}">
             <i class="fas fa-cog me-2"></i>Jasa Servis</p>
           </a>
         </div>
